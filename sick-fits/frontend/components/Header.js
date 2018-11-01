a0a9222b4bec8d73b173b4c1217ac85fa3005251
Sree <harsha6raju@yahoo.com>
2018-11-01 12:23:47 -0400

    add styling to logo

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,14 +1,24 @@
 import Nav from './Nav'
 import Link from 'next/link';
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+    font-size: 4rem;
+    margin-left: 2rem;
+    position: relative;
+    z-index: 2;
+    transform: skew(-7deg);
+`;
+
 const Header = () => (
     <div>
         <div className="bar">
-            <h1>
+            <Logo>
                 <Link href="/">
                     <a>Sick Fits</a>
                 </Link>
-            </h1>
-            <Nav />
+            </Logo>
+            <Nav/>
         </div>
         <div className="sub-bar">
             <p>Search</p>
