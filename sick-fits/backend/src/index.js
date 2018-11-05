d19e15ab930923004fd110b1a3a16790e87aead8
Sree <harsha6raju@yahoo.com>
2018-11-05 12:51:30 -0500

    start server from index.js

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,26 @@
-// let's go!
+ // dotenv to access our env variables
+require('dotenv').config({path: 'variables.env'});
+
+//import graphqlyoga server
+const createServer = require('createServer');
+
+//import our db
+const db = require('./db');
+
+// invoke our server from server variable
+const server = createServer();
+
+//TODO use express middleware to handle cookies (JWT)
+// TODO use express middleware to poppulate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    }
+}, deets => { 
+    console.log(`the server is now runnning on http://localhost:${deets.port}`)
+});
+
+
+
