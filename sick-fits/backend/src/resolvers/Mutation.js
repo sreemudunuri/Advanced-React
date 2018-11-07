3dd84eb76ef5d8435dcebcaa348a5f46e081c7a6
Sree <harsha6raju@yahoo.com>
2018-11-07 09:37:17 -0500

    testing resolvers and graphql schema

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info){
+
+        //local storage 
+         global.dogs = global.dogs || []
+        // create a dog
+        const newDog = {name: args.name}
+        
+        global.dogs.push(newDog)
+
+        return newDog
+    },
+};
 
 module.exports = Mutations;
