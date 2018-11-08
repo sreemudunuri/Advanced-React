266cea1c356f991c59750bbcb18b3b303f0af0ae
Sree <harsha6raju@yahoo.com>
2018-11-08 18:55:20 -0500

    comment createdAt and updatedAt in schema

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,14 +1,14 @@
 const Mutations = {
-    createDog(parent, args, ctx, info){
+    async createItem(parent, args, ctx, info) {
+        // access db
+        // ctx will return a prmoise 
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            },
+        }, info);
 
-        //local storage 
-         global.dogs = global.dogs || []
-        // create a dog
-        const newDog = {name: args.name}
-        
-        global.dogs.push(newDog)
-
-        return newDog
+        return item;
     },
 };
 
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,5 @@
 const Query = {
-    dogs(parent, args, ctx, info){
-        global.dogs = global.dogs || []
-        return global.dogs
-    },
+  
 };
 
 module.exports = Query;
